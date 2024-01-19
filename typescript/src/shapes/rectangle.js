e9e93f771ca83bf035c7590aa8143e3898855dbe
deepakbhimavarapu <deepakbhimavarapu@msitprogram.net>
2024-01-19 11:10:57 +0530

    TS code is completed

---
+++ typescript/src/shapes/rectangle.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rectangle = void 0;
+function rectangle(width, height) {
+    return {
+        computeArea: function () {
+            return width * height;
+        }
+    };
+}
+exports.rectangle = rectangle;
--- typescript/src/shapes/square.ts
+++ typescript/src/shapes/square.ts
@@ -1,6 +1,6 @@
+import { Shape } from './shape.js';
 function square(sideLen: number): Shape {
     return {
-        sideLen,
         computeArea: function (): number {
             return sideLen * sideLen
         }
